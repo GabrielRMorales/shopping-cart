53ce804fe6508d1d87f6d9951471e8a4a09fecc4
GabrielRMorales <grmorale@gmail.com>
2021-06-06 12:28:02 -0700

    Further style the app

--- src/Product.js
+++ src/Product.js
@@ -4,17 +4,22 @@ const Product = (props)=>{
     const [quantity, setQuantity] = useState(0);
 
     return (<div role="product" className="product">
+        <img src="" class="product-img" />
+        <h1 class="product-title"></h1>
+        <p class="product-description"></p>
         <p>{quantity}</p>
+        <section class="controls">
     <button role="increase" className="increase" onClick={()=>setQuantity(quantity+1)} >+</button>
     <button role="decrease" className="decrease" onClick={()=>{
         if (quantity>0){
             setQuantity(quantity-1);
         }
     }}>–</button>
-    <button role="add-to-cart" onClick={()=>{
+    <button role="add-to-cart" class="add-to-cart" onClick={()=>{
         props.onClick(quantity)
         setQuantity(0);
         }}><img src="./cart.svg" /><p>Add to Cart</p></button>
+        </section>
     </div>);
 }
 
--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const ShoppingCart = (props)=>{
     return (<div role="shopping-cart" id="shopping-cart">
-        <div role="total-items"><img src="./cart.svg" /><p>{props.totalItems || 0}</p></div>
+        <section id="project-brand">GAMECO</section><div role="total-items" id="total-items"><img src="./cart.svg" /><p>{props.totalItems || 0}</p></div>
     </div>);
 }
 
